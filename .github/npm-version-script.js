226dfe2cff65e71b4946e6a72f71cd64cd379b6c
Ben <43026681+bwp91@users.noreply.github.com>
2023-10-29 12:06:55 +0000

    Update npm-version-script.js

--- .github/npm-version-script.js
+++ .github/npm-version-script.js
@@ -30,8 +30,13 @@ if (refArgument == null) {
  */
 function getTagVersionFromNpm(tag) {
   try {
+    // once-only workaround for homebridge-config-ui-x which we are renaming the test tag to beta
+    if (packageJSON.name === "homebridge-config-ui-x" && tag === "test") {
+      tag = "beta";
+    }
     return child_process.execSync(`npm info ${packageJSON.name} version --tag="${tag}"`).toString("utf8").trim();
   } catch (e) {
+    console.error(`Failed to query the npm registry for the latest version for tag: ${tag}`);
     throw e;
   }
 }
